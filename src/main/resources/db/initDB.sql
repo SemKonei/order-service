32898defdee3c34683cc075baf7558d72e425ea8
noname_user <shyshoggoth@gmail.com>
2022-07-13 22:36:28 +0300

    add new price constraints

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -9,36 +9,36 @@ CREATE SEQUENCE global_seq START WITH 100000;
 CREATE TABLE users
 (
     id         BIGINT    DEFAULT NEXT VALUE FOR global_seq PRIMARY KEY,
-    name       VARCHAR(100)            NOT NULL,
-    email      VARCHAR(100)            NOT NULL,
-    password   VARCHAR(100)            NOT NULL,
+    name       VARCHAR                 NOT NULL,
+    email      VARCHAR                 NOT NULL,
+    password   VARCHAR                 NOT NULL,
     registered TIMESTAMP DEFAULT now() NOT NULL,
     enabled    BOOL      DEFAULT TRUE  NOT NULL
 );
 
 CREATE TABLE merch
 (
-    id         BIGINT    DEFAULT NEXT VALUE FOR global_seq PRIMARY KEY,
+    id         BIGINT DEFAULT NEXT VALUE FOR global_seq PRIMARY KEY,
     name       TEXT  NOT NULL,
-    curr_price FLOAT NOT NULL
+    curr_price FLOAT NOT NULL CHECK (curr_price > 0)
 );
 
 CREATE TABLE orders
 (
     id        BIGINT    DEFAULT NEXT VALUE FOR global_seq PRIMARY KEY,
-    user_id   INTEGER                           NOT NULL,
-    date_time TIMESTAMP           DEFAULT now() NOT NULL,
+    user_id   INTEGER                 NOT NULL,
+    date_time TIMESTAMP DEFAULT now() NOT NULL,
     /*price    FLOAT   NOT NULL,*/
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
 CREATE TABLE order_merch
 (
-    id       BIGINT    DEFAULT NEXT VALUE FOR global_seq PRIMARY KEY,
+    id       BIGINT DEFAULT NEXT VALUE FOR global_seq PRIMARY KEY,
     order_id INTEGER NOT NULL,
     merch_id INTEGER NOT NULL,
-    price    FLOAT   NOT NULL,
-    count    INTEGER NOT NULL,
+    price    FLOAT   NOT NULL CHECK (price > 0),
+    count    INTEGER NOT NULL CHECK (count > 0),
     FOREIGN KEY (order_id) REFERENCES orders (id) ON DELETE CASCADE,
     FOREIGN KEY (merch_id) REFERENCES merch (id) ON DELETE CASCADE
 );
