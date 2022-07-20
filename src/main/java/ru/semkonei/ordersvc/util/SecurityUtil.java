74a677f224f74ab1b92eab5d8e7f55dfe4d6aba9
noname_user <shyshoggoth@gmail.com>
2022-07-20 22:34:53 +0300

    change default userId

--- src/main/java/ru/semkonei/ordersvc/util/SecurityUtil.java
+++ src/main/java/ru/semkonei/ordersvc/util/SecurityUtil.java
@@ -2,7 +2,7 @@ package ru.semkonei.ordersvc.util;
 
 public class SecurityUtil {
 
-    private static Integer authUserId = 1;
+    private static Integer authUserId = 100000;
 
     public static Integer authUserId() {
         return authUserId;
