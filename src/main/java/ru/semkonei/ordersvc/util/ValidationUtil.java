45565788ce7b21746c75c95237d75880c64dcd16
noname_user <shyshoggoth@gmail.com>
2022-07-20 22:55:16 +0300

    change BaseEntity to HasId in assureIdConsistent method

--- src/main/java/ru/semkonei/ordersvc/util/ValidationUtil.java
+++ src/main/java/ru/semkonei/ordersvc/util/ValidationUtil.java
@@ -1,6 +1,7 @@
 package ru.semkonei.ordersvc.util;
 
 import ru.semkonei.ordersvc.model.BaseEntity;
+import ru.semkonei.ordersvc.model.HasId;
 import ru.semkonei.ordersvc.util.exception.NotFoundException;
 
 public class ValidationUtil {
@@ -31,7 +32,7 @@ public class ValidationUtil {
         }
     }
 
-    public static void assureIdConsistent(BaseEntity entity, int id) {
+    public static void assureIdConsistent(HasId entity, int id) {
         if (entity.isNew()) {
             entity.setId(id);
         } else if (entity.id() != id) {
