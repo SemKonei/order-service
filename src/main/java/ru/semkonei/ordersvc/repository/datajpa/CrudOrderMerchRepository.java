5f21bc3f75f8aa95fb1146c716f506fd36ba905f
noname_user <shyshoggoth@gmail.com>
2022-07-29 22:04:58 +0300

    fix deleting OrderMerch from db

--- src/main/java/ru/semkonei/ordersvc/repository/datajpa/CrudOrderMerchRepository.java
+++ src/main/java/ru/semkonei/ordersvc/repository/datajpa/CrudOrderMerchRepository.java
@@ -11,13 +11,19 @@ import java.util.List;
 
 @Transactional(readOnly = true)
 public interface CrudOrderMerchRepository extends JpaRepository<OrderMerch, Integer> {
-
     @Transactional
     @Modifying
-    @Query("DELETE FROM OrderMerch o WHERE o.id=:id AND o.order.user.id=:userId")
-    int delete(@Param("id") int id, @Param("userId") int userId);
+//    @Query(value = "DELETE FROM OrderMerch om WHERE om.id=:id AND om.order.user.id=:userId") не работает, но может когда-нибудь что-то изменится
+    @Query(value = "DELETE FROM order_merch " +
+            "WHERE order_merch.id=:id " +
+            "AND order_merch.order_id=:orderId " +
+            "AND :orderId IN" +
+                    " (SELECT o.id FROM orders o" +
+                    " WHERE o.user_id=:userId)",
+            nativeQuery = true)
+    int delete(@Param("id") int id, @Param("orderId") int orderId, @Param("userId") int userId);
 
-    @Query("SELECT o FROM OrderMerch o WHERE o.order.id=:orderId AND o.order.user.id=:userId")
+    @Query("SELECT o FROM OrderMerch o  WHERE o.order.id=:orderId AND o.order.user.id=:userId")
     List<OrderMerch> getAll(@Param("orderId") int orderId, @Param("userId") int userId);
 
 }
